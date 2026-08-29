d551f19cc186de264e445d831c0f69e0360a1480
soenneker <jake@soenneker.com>
2026-08-29 14:16:57 -0500

    Improve public API XML documentation

--- src/Soenneker.Context7.HttpClients/Abstract/IContext7OpenApiHttpClient.cs
+++ src/Soenneker.Context7.HttpClients/Abstract/IContext7OpenApiHttpClient.cs
@@ -2,13 +2,16 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Context7.HttpClients.Abstract;
-
 /// <summary>
 /// A .NET thread-safe singleton HttpClient for 
 /// </summary>
 public interface IContext7OpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Context7 OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Context7.HttpClients/Registrars/Context7OpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Context7.HttpClients/Registrars/Context7OpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class Context7OpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="Context7OpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddContext7OpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class Context7OpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="Context7OpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddContext7OpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
